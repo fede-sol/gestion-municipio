cfdbcc650f544e68b896d1f20db93fad87619ab2
fede-sol <fedesolanes1@gmail.com>
2024-06-09 17:54:52 -0300

    Add GMUserAdmin to admin.py

--- main_app/admin.py
+++ main_app/admin.py
@@ -5,7 +5,6 @@ admin.site.register(Barrio)
 admin.site.register(Desperfecto)
 admin.site.register(Rubro)
 admin.site.register(Vecino)
-admin.site.register(GMUser)
 admin.site.register(Personal)
 admin.site.register(Sitio)
 admin.site.register(Reclamo)
@@ -13,3 +12,25 @@ admin.site.register(ImagenReclamo)
 admin.site.register(ImagenPromocion)
 admin.site.register(Promocion)
 admin.site.register(UserRegisterCode)
+
+
+class GMUserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'get_user_type', 'get_user_info')
+
+    def get_user_type(self, obj):
+        return obj.get_user_type_display()
+    get_user_type.short_description = 'User Type'
+
+    def get_user_info(self, obj):
+        if obj.user_type == 1:  # Vecino
+            vecino = Vecino.objects.filter(usuario=obj).first()
+            if vecino:
+                return f"Documento: {vecino.documento}, Nombre: {vecino.nombre}"
+        elif obj.user_type == 2:  # Personal
+            personal = Personal.objects.filter(usuario=obj).first()
+            if personal:
+                return f"Legajo: {personal.legajo}, Nombre: {personal.nombre}"
+        return "-"
+    get_user_info.short_description = 'User Info'
+
+admin.site.register(GMUser, GMUserAdmin)
