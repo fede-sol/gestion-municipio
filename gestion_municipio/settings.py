d1500dcf19d018fd9ac8977c0f19cc328521e17c
facupedaccio <fpedaccio@gmail.com>
2024-07-01 13:54:19 -0300

    rolly rolly rolly

--- gestion_municipio/settings.py
+++ gestion_municipio/settings.py
@@ -81,15 +81,10 @@ WSGI_APPLICATION = 'gestion_municipio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'PZGHkSMmPwuPWxrzMwXyFKAhePcjXdxX',
-        'HOST': 'monorail.proxy.rlwy.net',
-        'PORT': '53909',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 
 # Password validation
